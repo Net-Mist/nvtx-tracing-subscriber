5d4ee71f78cdff87e15ef886de192bfa8db712d6
Sébastien Iooss <archimist.linux@gmail.com>
2024-11-13 22:10:13 +0100

    add Layer implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use tracing::{span, Event, Id, Metadata, Subscriber};
-// use tracing_subscriber::layer::Context;
+use tracing_subscriber::layer::Context;
+// use tracing_subscriber::prelude::*;
+use tracing_subscriber::Layer;
 
-use nvtx::{range_pop, range_push, mark};
+use nvtx::{mark, range_pop, range_push};
 use std::sync::{
     atomic::{AtomicUsize, Ordering},
     Mutex,
@@ -66,6 +68,47 @@ impl Subscriber for NvtxSubscriber {
     }
 
     fn exit(&self, _span: &Id) {
+        range_pop!();
+    }
+}
+
+pub struct NvtxLayer {
+    spans: Mutex<Vec<Span>>,
+}
+
+impl NvtxLayer {
+    pub fn new() -> Self {
+        Self {
+            spans: Mutex::new(Vec::new()),
+        }
+    }
+}
+
+impl<S: Subscriber> Layer<S> for NvtxLayer {
+    fn on_new_span(&self, attrs: &span::Attributes<'_>, id: &span::Id, ctx: Context<'_, S>) {
+        let _ = (id, ctx);
+
+        let name = attrs.metadata().name();
+
+        let mut lock = self.spans.lock().unwrap();
+        lock.push(Span {
+            id: id.clone(),
+            name: name.to_string(),
+        });
+    }
+
+    fn on_event(&self, event: &Event<'_>, _ctx: Context<'_, S>) {
+        mark!("{:?}", event);
+    }
+
+    fn on_enter(&self, id: &Id, _ctx: Context<'_, S>) {
+        let lock = self.spans.lock().unwrap();
+        let span = lock.iter().find(|s| s.id == *id).unwrap();
+        let name = &span.name;
+        range_push!("{}", name);
+    }
+
+    fn on_exit(&self, _id: &Id, _ctx: Context<'_, S>) {
         range_pop!();
     }
 }
